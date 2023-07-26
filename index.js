1b93ec9b655797fdae4d42a2772b7667177851c0
ChibuikeNwankwod <chibuikenwankwod@gmail.com>
2023-07-26 23:21:55 +0100

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-let det = get;
+const MenuIcon = document.querySelector(".menu-icon");
+const MenuContent = document.querySelector(".mobile-menu-content");
+const Children = document.querySelectorAll(".mobile-menu-content > li");
+const close = document.getElementById("close-menu");
 
-console.log(det);
+console.log(Children);
+
+MenuIcon.addEventListener("click", () => {
+  MenuContent.classList.add("show-menu");
+});
+
+close.addEventListener("click", () => {
+  MenuContent.classList.remove("show-menu");
+});
+
+// const phoneInputField = document.getElementById("#phone");
+// const phoneInput = window.intlTelInput(phoneInputField, {
+//   utilsScript:
+//     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+// });
